631818587f9525e84ffd4e55dbdacc5eb7ac5e92
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-08-13 18:21:26 +0900

    レイアウトをした

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <v-app-bar app dense>
+      <v-app-bar-nav-icon @click="isDrawerEnabled = !isDrawerEnabled" />
+      <v-toolbar-title>Dashboard</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="isDrawerEnabled" app>
+      <v-list dense nav>
+        <v-list-item></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class NavBar extends Vue {
+  isDrawerEnabled: boolean = false
+}
+</script>
